b534fa610052a98b0eecaac02416ecd855ea8235
Firebird492 <gavinbuier@outlook.com>
2024-09-20 17:02:17 -0400

    add dict conversion to handle mongo queries

--- BackEnd/models.js
+++ BackEnd/models.js
@@ -1,50 +1,75 @@
 class MenuItem{
-    constructor(id, name, price, allergen, description, image) {
-        this.id = id;
-        this.name = name;
-        this.price = price;
-        this.allergen = allergen;
-        this.description = description;
-        this.image = image;
+    constructor(data) {
+        this.id = data.id;
+        this.name = data.name;
+        this.price = data.price;
+        this.allergen = data.allergen;
+        this.description = data.description;
+        this.image = data.image;
+    }
+
+    convertToDict(){
+        return {
+            id : this.id, name : this.name, price : this.price, allergen : this.allergen, 
+            description : this.description, image : this.image
+        }
     }
 }
 
 
 class Order{
-    constructor(id, accountId, orderTime, pickupLocation, items, costOfItems, tip, completed) {
-        this.id = id;
-        this.accountId = accountId;
-        this.orderTime = orderTime;
-        this.pickupLocation = pickupLocation;
-        this.items = items;
-        this.costOfItems = costOfItems;
-        this.tip = tip;
-        this.completed = completed;
+    constructor(data) {
+        this.id = data.id;
+        this.accountId = data.accountId;
+        this.orderTime = data.orderTime;
+        this.pickupLocation = data.pickupLocation;
+        this.items = data.items;
+        this.costOfItems = data.costOfItems;
+        this.tip = data.tip;
+        this.completed = data.completed;
     }
 
     getTotalCost() {
         return this.tip + this.costOfItems;
     }
+
+    convertToDict() {
+        return {
+            id : this.id, accountId : this.accountId, orderTime : this.orderTime, pickupLocation : this.pickupLocation,
+            items : this.items, costOfItems : this.costOfItems, tip : this.tip, completed : this.completed
+        }
+    }
 }
 
 
 class Account{
-    constructor(id, name, email, phone, accessLevel, cart) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.phone = phone;
-        this.accessLevel = accessLevel;
-        this.cart = cart;
+    constructor(data) {
+        this.id = data.id;
+        this.name = data.name;
+        this.email = data.email;
+        this.phone = data.phone;
+        this.accessLevel = data.accessLevel;
+        this.cart = data.cart;
+    }
+
+    convertToDict() {
+        return {
+            id : this.id, name : this.name, email : this.email, phone : this.phone, 
+            accessLevel : this.accessLevel, cart : this.cart
+        }
     }
 }
 
 
 class PickupLocation{
-    constructor(id, address, contactInfo, name){
-        this.id = id;
-        this.address = address;
-        this.contactInfo = contactInfo;
-        this.name = name;
+    constructor(data) {
+        this.id = data.id;
+        this.address = data.address;
+        this.contactInfo = data.contactInfo;
+        this.name = data.name;
+    }
+
+    convertToDict() {
+        return {id : this.id, address : this.address, contactInfo : this.contactInfo, name : this.name}
     }
 }
