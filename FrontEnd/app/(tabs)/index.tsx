62bf9a92965f1cbcf37b60ab184a9ce16bb3d42b
DJCoderMan <realjdu.01@gmail.com>
2024-09-24 16:59:06 -0400

    testing

--- FrontEnd/app/(tabs)/index.tsx
+++ FrontEnd/app/(tabs)/index.tsx
@@ -22,7 +22,7 @@ export default function HomeScreen() {
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Step 1: Try it</ThemedText>
         <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
+          Hey!!!!!
           Press{' '}
           <ThemedText type="defaultSemiBold">
             {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
